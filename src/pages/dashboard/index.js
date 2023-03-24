21613cc952829fabd98d1c426757233e65656c1d
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-24 22:12:02 +0700

    Add title to head tag

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,141 +1,151 @@
 import Layout from "@/components/layout";
 import NavBar from "@/components/navbar";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Dashboard({ result }) {
   return (
-    <Layout>
-      <NavBar />
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-              <th scope="col" className="px-6 py-3 sticky left-0 bg-gray-50">
-                Company Logo
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
-                  Title
-                  <a href="#">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
-                    >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </a>
-                </div>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
-                  Company name
-                  <a href="#">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
-                    >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </a>
-                </div>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
-                  Company city
-                  <a href="#">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
-                    >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </a>
-                </div>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
-                  Job status
-                  <a href="#">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3 ml-1"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 320 512"
-                    >
-                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                    </svg>
-                  </a>
-                </div>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">Action</div>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {result.data.map((el) => (
-              <tr key={el.id} className="bg-white border-b">
-                <td
-                  scope="row"
-                  className="px-6 sticky left-0 py-4 font-medium text-gray-900 whitespace-nowrap bg-white"
-                >
-                  <img className="rounded shadow-md" width={75} src={el.company_image_url}></img>
-                </td>
-                <td className="px-6 py-4">{el.title}</td>
-                <td className="px-6 py-4">{el.company_name}</td>
-                <td className="px-6 py-4">{el.company_city}</td>
-                <td className="px-6 py-4">
-                  {el.job_status === 1 ? "Open" : "Closed"}
-                </td>
-                <td className="px-6 py-4 text-left">
-                  <Link
-                    href={`/dashboard/list-job-vacancy/edit/${el.id}`}
-                    className="font-medium mr-6 text-blue-600 hover:underline"
-                  >
-                    Edit
-                  </Link>
-                  <a
-                    href="#"
-                    className="font-medium text-red-600 hover:underline"
-                  >
-                    Delete
-                  </a>
-                </td>
+    <>
+    <Head>
+      <title>Vakanzie - Dashboard</title>
+    </Head>
+      <Layout>
+        <NavBar />
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-3 sticky left-0 bg-gray-50">
+                  Company Logo
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <div className="flex items-center">
+                    Title
+                    <a href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <div className="flex items-center">
+                    Company name
+                    <a href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <div className="flex items-center">
+                    Company city
+                    <a href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <div className="flex items-center">
+                    Job status
+                    <a href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-3 h-3 ml-1"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 320 512"
+                      >
+                        <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                      </svg>
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <div className="flex items-center">Action</div>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <span className="sr-only">Edit</span>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="absolute bottom-5 right-5">
-        <Link href="/dashboard/list-job-vacancy/create">
-          <svg
-            width="40"
-            height="40"
-            viewBox="0 0 100 100"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M49.9999 70.8333C51.1805 70.8333 52.1708 70.4333 52.9708 69.6333C53.7708 68.8333 54.1694 67.8445 54.1666 66.6667V54.1667H66.7708C67.9513 54.1667 68.9235 53.7667 69.6874 52.9667C70.4513 52.1667 70.8333 51.1778 70.8333 50C70.8333 48.8195 70.4333 47.8292 69.6333 47.0292C68.8333 46.2292 67.8444 45.8306 66.6666 45.8333H54.1666V33.2292C54.1666 32.0486 53.7666 31.0764 52.9666 30.3125C52.1666 29.5486 51.1777 29.1667 49.9999 29.1667C48.8194 29.1667 47.8291 29.5667 47.0291 30.3667C46.2291 31.1667 45.8305 32.1556 45.8333 33.3333V45.8333H33.2291C32.0485 45.8333 31.0763 46.2333 30.3124 47.0333C29.5485 47.8333 29.1666 48.8222 29.1666 50C29.1666 51.1806 29.5666 52.1708 30.3666 52.9708C31.1666 53.7708 32.1555 54.1695 33.3333 54.1667H45.8333V66.7708C45.8333 67.9514 46.2333 68.9236 47.0333 69.6875C47.8333 70.4514 48.8221 70.8333 49.9999 70.8333ZM49.9999 91.6667C44.236 91.6667 38.8194 90.5722 33.7499 88.3833C28.6805 86.1945 24.2708 83.2264 20.5208 79.4792C16.7708 75.7292 13.8027 71.3195 11.6166 66.25C9.43047 61.1806 8.33603 55.7639 8.33325 50C8.33325 44.2361 9.4277 38.8195 11.6166 33.75C13.8055 28.6806 16.7735 24.2708 20.5208 20.5208C24.2708 16.7708 28.6805 13.8028 33.7499 11.6167C38.8194 9.43057 44.236 8.33612 49.9999 8.33334C55.7638 8.33334 61.1805 9.42779 66.2499 11.6167C71.3194 13.8056 75.7291 16.7736 79.4791 20.5208C83.2291 24.2708 86.1985 28.6806 88.3874 33.75C90.5763 38.8195 91.6694 44.2361 91.6666 50C91.6666 55.7639 90.5721 61.1806 88.3833 66.25C86.1944 71.3195 83.2263 75.7292 79.4791 79.4792C75.7291 83.2292 71.3194 86.1986 66.2499 88.3875C61.1805 90.5764 55.7638 91.6695 49.9999 91.6667Z"
-              fill="#212121"
-            />
-          </svg>
-        </Link>
-      </div>
-    </Layout>
+            </thead>
+            <tbody>
+              {result.data.map((el) => (
+                <tr key={el.id} className="bg-white border-b">
+                  <td
+                    scope="row"
+                    className="px-6 sticky left-0 py-4 font-medium text-gray-900 whitespace-nowrap bg-white"
+                  >
+                    <img
+                      className="rounded shadow-md"
+                      width={75}
+                      src={el.company_image_url}
+                    ></img>
+                  </td>
+                  <td className="px-6 py-4">{el.title}</td>
+                  <td className="px-6 py-4">{el.company_name}</td>
+                  <td className="px-6 py-4">{el.company_city}</td>
+                  <td className="px-6 py-4">
+                    {el.job_status === 1 ? "Open" : "Closed"}
+                  </td>
+                  <td className="px-6 py-4 text-left">
+                    <Link
+                      href={`/dashboard/list-job-vacancy/edit/${el.id}`}
+                      className="font-medium mr-6 text-blue-600 hover:underline"
+                    >
+                      Edit
+                    </Link>
+                    <a
+                      href="#"
+                      className="font-medium text-red-600 hover:underline"
+                    >
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="absolute bottom-5 right-5">
+          <Link href="/dashboard/list-job-vacancy/create">
+            <svg
+              width="40"
+              height="40"
+              viewBox="0 0 100 100"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M49.9999 70.8333C51.1805 70.8333 52.1708 70.4333 52.9708 69.6333C53.7708 68.8333 54.1694 67.8445 54.1666 66.6667V54.1667H66.7708C67.9513 54.1667 68.9235 53.7667 69.6874 52.9667C70.4513 52.1667 70.8333 51.1778 70.8333 50C70.8333 48.8195 70.4333 47.8292 69.6333 47.0292C68.8333 46.2292 67.8444 45.8306 66.6666 45.8333H54.1666V33.2292C54.1666 32.0486 53.7666 31.0764 52.9666 30.3125C52.1666 29.5486 51.1777 29.1667 49.9999 29.1667C48.8194 29.1667 47.8291 29.5667 47.0291 30.3667C46.2291 31.1667 45.8305 32.1556 45.8333 33.3333V45.8333H33.2291C32.0485 45.8333 31.0763 46.2333 30.3124 47.0333C29.5485 47.8333 29.1666 48.8222 29.1666 50C29.1666 51.1806 29.5666 52.1708 30.3666 52.9708C31.1666 53.7708 32.1555 54.1695 33.3333 54.1667H45.8333V66.7708C45.8333 67.9514 46.2333 68.9236 47.0333 69.6875C47.8333 70.4514 48.8221 70.8333 49.9999 70.8333ZM49.9999 91.6667C44.236 91.6667 38.8194 90.5722 33.7499 88.3833C28.6805 86.1945 24.2708 83.2264 20.5208 79.4792C16.7708 75.7292 13.8027 71.3195 11.6166 66.25C9.43047 61.1806 8.33603 55.7639 8.33325 50C8.33325 44.2361 9.4277 38.8195 11.6166 33.75C13.8055 28.6806 16.7735 24.2708 20.5208 20.5208C24.2708 16.7708 28.6805 13.8028 33.7499 11.6167C38.8194 9.43057 44.236 8.33612 49.9999 8.33334C55.7638 8.33334 61.1805 9.42779 66.2499 11.6167C71.3194 13.8056 75.7291 16.7736 79.4791 20.5208C83.2291 24.2708 86.1985 28.6806 88.3874 33.75C90.5763 38.8195 91.6694 44.2361 91.6666 50C91.6666 55.7639 90.5721 61.1806 88.3833 66.25C86.1944 71.3195 83.2263 75.7292 79.4791 79.4792C75.7291 83.2292 71.3194 86.1986 66.2499 88.3875C61.1805 90.5764 55.7638 91.6695 49.9999 91.6667Z"
+                fill="#212121"
+              />
+            </svg>
+          </Link>
+        </div>
+      </Layout>
+    </>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
 import NavBar from "@/components/navbar";
+import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -9,109 +10,114 @@ export default function Home({ jobData }) {
     setSearch(e.target.value);
   };
   return (
-    <Layout>
-      <NavBar />
-      <div className="h-[75vh] flex justify-center flex-col items-center">
-        <p className="text-4xl font-bold text-center mb-10 md:text-6xl md:w-[500px]">
-          Get the <span className="text-blue-700">Right Job</span> You Deserve
-        </p>
-        <p className="text-center mb-10">
-          Million jobs listed here! You're dream job is waiting.
-        </p>
-        <form>
-          <label
-            for="default-search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only"
-          >
-            Search
-          </label>
-          <div class="relative">
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                class="w-5 h-5 text-gray-500"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="search"
-              name="search"
-              id="default-search"
-              className="block w-80 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Search jobs position..."
-              value={search}
-              onChange={handleChange}
-              required
-            />
-            {/* <button
+    <>
+    <Head>
+      <title>Vakanzie - Home</title>
+    </Head>
+      <Layout>
+        <NavBar />
+        <div className="h-[75vh] flex justify-center flex-col items-center">
+          <p className="text-4xl font-bold text-center mb-10 md:text-6xl md:w-[500px]">
+            Get the <span className="text-blue-700">Right Job</span> You Deserve
+          </p>
+          <p className="text-center mb-10">
+            Million jobs listed here! You're dream job is waiting.
+          </p>
+          <form>
+            <label
+              for="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only"
+            >
+              Search
+            </label>
+            <div class="relative">
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  class="w-5 h-5 text-gray-500"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                name="search"
+                id="default-search"
+                className="block w-80 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Search jobs position..."
+                value={search}
+                onChange={handleChange}
+                required
+              />
+              {/* <button
               type="submit"
               className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2"
             >
               Search
             </button> */}
-          </div>
-        </form>
-      </div>
-      <main className="grid mx-auto gap-5 mt-5 md:grid-cols-3">
-        {jobData
-          .filter((item) => {
-            return search.toLowerCase() === ""
-              ? item
-              : item.title.toLowerCase().includes(search.toLowerCase());
-          })
-          .map((item) => (
-            <div>
-              <Link
-                href={`/dashboard/list-job-vacancy/detail/${item.id}`}
-                className="flex flex-col p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 h-[350px]"
-              >
-                <div className="flex justify-between mb-3 items-top">
-                  <div className="mr-3">
-                    <h5 className="mb-2 text-sm tracking-tight text-gray-400">
-                      {item.company_name}
-                    </h5>
-                    <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">
-                      {item.title}
-                    </h5>
-                    <h5 className="mb-2 text-sm font-bold tracking-tight text-blue-500">
-                      {`${item.salary_min} IDR - ${item.salary_max} IDR`}
-                    </h5>
-                  </div>
-                  <img
-                    className="rounded-xl shadow-md h-14"
-                    src={item.company_image_url}
-                    alt="company logo"
-                  />
-                </div>
-
-                <p className="font-normal mb-10 text-sm text-gray-500 flex-grow">
-                  {item.job_description.substring(0, 250) + "..."}
-                </p>
-                <div className="flex justify-between items-center">
-                  <p className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-xs px-5 py-2.5 text-center">
-                    {item.job_tenure.toUpperCase()}
-                  </p>
-                  <p>{item.company_city}</p>
-                </div>
-              </Link>
             </div>
-          ))}
-      </main>
-    </Layout>
+          </form>
+        </div>
+        <main className="grid mx-auto gap-5 mt-5 md:grid-cols-3">
+          {jobData
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.title.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((item) => (
+              <div>
+                <Link
+                  href={`/dashboard/list-job-vacancy/detail/${item.id}`}
+                  className="flex flex-col p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 h-[350px]"
+                >
+                  <div className="flex justify-between mb-3 items-top">
+                    <div className="mr-3">
+                      <h5 className="mb-2 text-sm tracking-tight text-gray-400">
+                        {item.company_name}
+                      </h5>
+                      <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">
+                        {item.title}
+                      </h5>
+                      <h5 className="mb-2 text-sm font-bold tracking-tight text-blue-500">
+                        {`${item.salary_min} IDR - ${item.salary_max} IDR`}
+                      </h5>
+                    </div>
+                    <img
+                      className="rounded-xl shadow-md h-14"
+                      src={item.company_image_url}
+                      alt="company logo"
+                    />
+                  </div>
+
+                  <p className="font-normal mb-10 text-sm text-gray-500 flex-grow">
+                    {item.job_description.substring(0, 250) + "..."}
+                  </p>
+                  <div className="flex justify-between items-center">
+                    <p className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-xs px-5 py-2.5 text-center">
+                      {item.job_tenure.toUpperCase()}
+                    </p>
+                    <p>{item.company_city}</p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+        </main>
+      </Layout>
+    </>
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const res = await fetch(
     "https://dev-example.sanbercloud.com/api/job-vacancy"
   );
