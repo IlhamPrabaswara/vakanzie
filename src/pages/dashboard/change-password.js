fcc3e6bc4a46dec60411aa68fe510294854b8349
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-25 14:05:06 +0700

    Add change password feature

--- src/pages/dashboard/change-password.js
+++ src/pages/dashboard/change-password.js
@@ -1,52 +1,97 @@
 import Layout from "@/components/layout";
 import NavBar from "@/components/navbar";
+import { useState } from "react";
+import Cookies from "js-cookie";
 
 export default function ChangePassword() {
+  const [input, setInput] = useState({
+    current_password: "",
+    new_password: "",
+    new_confirm_password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((input) => ({
+      ...input,
+      [name]: value,
+    }));
+  };
+  const handleUpdatePassword = async (e) => {
+    e.preventDefault();
+    const data = {
+      current_password: input.current_password,
+      new_password: input.new_password,
+      new_confirm_password: input.new_confirm_password,
+    };
+    const JSONdata = JSON.stringify(data);
+
+    const endpoint = `https://dev-example.sanbercloud.com/api/change-password`;
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+    const response = await fetch(endpoint, options);
+    const result = await response.json();
+    alert("Password is successfully updated")
+  };
   return (
     <Layout>
       <NavBar />
       <h1>Change Password</h1>
-      <form>
+      <form onSubmit={handleUpdatePassword}>
         <div class="mb-6">
           <label
-            for="email"
+            for="current_password"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Current password
           </label>
           <input
-            type="email"
-            id="email"
+            type="password"
+            name="current_password"
+            id="current_password"
             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             placeholder="name@flowbite.com"
+            value={input.current_password}
+            onChange={handleChange}
             required
           />
         </div>
         <div class="mb-6">
           <label
-            for="password"
+            for="new_password"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             New password
           </label>
           <input
             type="password"
-            id="password"
+            id="new_password"
+            name="new_password"
             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            value={input.new_password}
+            onChange={handleChange}
             required
           />
         </div>
         <div class="mb-6">
           <label
-            for="repeat-password"
+            for="new_confirm_password"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Repeat new password
           </label>
           <input
             type="password"
-            id="repeat-password"
+            id="new_confirm_password"
             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            value={input.new_confirm_password}
+            name="new_confirm_password"
+            onChange={handleChange}
             required
           />
         </div>
