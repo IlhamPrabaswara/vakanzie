20e3e6715afdb704a98769834b7692e5c15630a3
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-02 00:48:32 +0700

    Remove dark mode class and change class attribute to className

--- src/pages/dashboard/change-password.js
+++ src/pages/dashboard/change-password.js
@@ -46,7 +46,7 @@ export default function ChangePassword() {
         <div class="mb-6">
           <label
             for="current_password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            class="block mb-2 text-sm font-medium text-gray-900"
           >
             Current password
           </label>
@@ -54,7 +54,7 @@ export default function ChangePassword() {
             type="password"
             name="current_password"
             id="current_password"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="name@flowbite.com"
             value={input.current_password}
             onChange={handleChange}
@@ -64,7 +64,7 @@ export default function ChangePassword() {
         <div class="mb-6">
           <label
             for="new_password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            class="block mb-2 text-sm font-medium text-gray-900"
           >
             New password
           </label>
@@ -72,7 +72,7 @@ export default function ChangePassword() {
             type="password"
             id="new_password"
             name="new_password"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             value={input.new_password}
             onChange={handleChange}
             required
@@ -81,14 +81,14 @@ export default function ChangePassword() {
         <div class="mb-6">
           <label
             for="new_confirm_password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            class="block mb-2 text-sm font-medium text-gray-900"
           >
             Repeat new password
           </label>
           <input
             type="password"
             id="new_confirm_password"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             value={input.new_confirm_password}
             name="new_confirm_password"
             onChange={handleChange}
@@ -97,7 +97,7 @@ export default function ChangePassword() {
         </div>
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
           Change password
         </button>
