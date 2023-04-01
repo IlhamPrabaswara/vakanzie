d0fad45a743bd8139b90e255a726f02c0ecdb650
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-02 00:49:36 +0700

    Change class attribute to className, remove dark mode.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer class="sticky top-full bg-white rounded-lg shadow mt-4 dark:bg-gray-800">
-      <div class="w-full mx-auto container p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className="sticky top-full bg-white rounded-lg shadow mt-4">
+      <div className="w-full mx-auto container p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center">
           © 2023{" "}
-          <a href="https://flowbite.com/" class="hover:underline">
+          <a href="https://flowbite.com/" className="hover:underline">
             Vakanzie™
           </a>
           . All Rights Reserved.
