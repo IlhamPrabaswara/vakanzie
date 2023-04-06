2860e19328e175699a974648b8f210c5049e2c66
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-06 16:03:27 +0700

    Add navbar to layout component and desktop view

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
 import Footer from "./footer";
+import NavBar from "./navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen px-5 pb-5">
-      {children}
-      <Footer />
-    </div>
+    <>
+      <div className="min-h-screen px-5 pb-5 mx-auto md:max-w-[1128px]">
+        <NavBar />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 }
