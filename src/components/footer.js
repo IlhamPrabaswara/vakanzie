9449569b0f9c1e6a976dd32dbb368d55ffc833b5
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-06 16:03:02 +0700

    Fix margin on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="sticky top-full bg-white rounded-lg shadow mt-4">
+    <footer className="sticky top-full bg-white rounded-lg shadow mt-4 md:mt-[50px]">
       <div className="w-full mx-auto container p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center">
           © 2023{" "}
