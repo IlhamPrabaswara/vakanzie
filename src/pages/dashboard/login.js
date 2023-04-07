56f1ad09daa93e729f1a3a364919c7bf3099a409
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-07 23:53:10 +0700

    Fix still login when invalid password

--- src/pages/dashboard/login.js
+++ src/pages/dashboard/login.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Cookies from "js-cookie";
 import Router from "next/router";
-import NavBar from "@/components/navbar";
 import Layout from "@/components/layout";
+import { DataContext } from "@/components/dataContext";
 
 export default function LogIn() {
   const [input, setInput] = useState({
@@ -12,13 +12,13 @@ export default function LogIn() {
     name: "",
     image_url: "",
   });
-
+  const { profilePicture, setProfilePicture } = useContext(DataContext);
   const handleChange = (e) => {
     let value = e.target.value;
     let name = e.target.name;
     setInput({ ...input, [name]: value });
   };
-  const handleRegister = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
 
     const data = {
@@ -37,16 +37,21 @@ export default function LogIn() {
       body: JSONdata,
     };
     const response = await fetch(endpoint, options);
-    const result = await response.json();
-    let { token } = result;
-    Cookies.set("token", token);
-    Router.push("/");
+    await response.json().then((result) => {
+      let { token } = result;
+      if (result.token !== undefined) {
+        Cookies.set("token", token);
+        Router.push("/");
+        localStorage.setItem('imageUrl', result.user.image_url)
+      } else {
+        alert("Password Invalid");
+      }
+    });
   };
   return (
     <Layout>
-      <NavBar />
-      <h1>Log In</h1>
-      <form className="mt-6" onSubmit={handleRegister}>
+      <h1>Log In as Recruiter</h1>
+      <form className="mt-6" onSubmit={handleLogin}>
         <div className="relative z-0 w-full mb-6 group">
           <input
             type="email"
@@ -60,7 +65,7 @@ export default function LogIn() {
           />
           <label
             htmlFor="floating_email"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="peer-focus:font-medium bg-white absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Email address
           </label>
@@ -91,7 +96,10 @@ export default function LogIn() {
         </button>
       </form>
       <p className="text-sm">
-        Don't have an account? <Link className="text-blue-700" href="/dashboard/signup">Sign up</Link>
+        Don't have an account?{" "}
+        <Link className="text-blue-700" href="/dashboard/signup">
+          Sign up
+        </Link>
       </p>
     </Layout>
   );
