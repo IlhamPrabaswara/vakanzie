5f4d97df36ce2035a14fd9a8b46777d912b23c8e
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-18 21:58:54 +0700

    Add new modal on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export default function Home({ jobData }) {
   const [search, setSearch] = useState("");
+  const [viewModal, setViewModal] = useState(true);
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
@@ -112,6 +113,45 @@ export default function Home({ jobData }) {
               </div>
             ))}
         </main>
+
+        <div
+          id="marketing-banner"
+          tabindex="-1"
+          className={`${
+            viewModal ? null : "hidden"
+          } fixed z-50 flex flex-col md:flex-row justify-between w-[calc(50%-2rem)] p-4 -translate-x-1/2 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl left-1/2 bottom-6`}
+        >
+          <div class="flex flex-col items-start mb-3 mr-4 md:items-center md:flex-row md:mb-0">
+            <p class="flex items-center text-sm font-normal text-gray-500">
+              This website is currently optimized for mobile only.
+            </p>
+          </div>
+          <div class="flex items-center flex-shrink-0">
+            <button
+              data-dismiss-target="#marketing-banner"
+              type="button"
+              class="absolute top-2.5 right-2.5 md:relative md:top-auto md:right-auto flex-shrink-0 inline-flex justify-center items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5"
+              onClick={() => {
+                setViewModal(!viewModal);
+              }}
+            >
+              <svg
+                aria-hidden="true"
+                class="w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span class="sr-only">Close banner</span>
+            </button>
+          </div>
+        </div>
       </Layout>
     </>
   );
