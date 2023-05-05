fbd120b25dfda1d113915c2f724c57deff67ddd5
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-05-05 19:31:00 +0700

    Change x padding for mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import NavBar from "./navbar";
 export default function Layout({ children }) {
   return (
     <>
-      <div className="min-h-screen px-5 pb-5 mx-auto md:max-w-[1128px]">
+      <div className="min-h-screen px-4 pb-5 mx-auto md:max-w-[1128px]">
         <NavBar />
         {children}
         <Footer />
