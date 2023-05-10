e7f1c06cde0295776c5a813d5756bd3536253d5a
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-05-10 23:22:07 +0700

    Remove email label background

--- src/pages/dashboard/login.js
+++ src/pages/dashboard/login.js
@@ -65,7 +65,7 @@ export default function LogIn() {
           />
           <label
             htmlFor="floating_email"
-            className="peer-focus:font-medium bg-white absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Email address
           </label>
